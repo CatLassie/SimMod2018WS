3b59c4e0b6fb1ac2a39ecf108d672a81b0695539
Peter_Ryabchenko <ryabchenko.peter@gmail.com>
2019-01-20 23:04:49 +0100

    added ground truth and ground truth loading / transormation for first episode

--- customScripts/utilities.py
+++ customScripts/utilities.py
@@ -12,6 +12,24 @@ def load_audio(file_path, sampling_rate, inf=False):
         print("sampling rate: ", sr, '\n\n\n')
     return y
 
+def load_ground_truth(file_path, delimiter):
+    gt = np.genfromtxt(fname=file_path, delimiter=delimiter)
+    return gt.astype(np.int64)
+
+def compute_0_padded_gt(gt, audio_length):
+    padded_gt = []
+    for i in range(audio_length):
+        padded_gt.append(0)
+    for i in range(len(gt)):
+        padded_gt[gt[i]-1] = 1
+    return padded_gt
+
+def adjust_gt_to_frames(gt, conversion_const):
+    converted_gt = []
+    for i, element in enumerate(gt):
+        for j in range(conversion_const):
+            converted_gt.append(element)
+    return converted_gt
 
 
 
